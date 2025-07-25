607f630492421d3088103c785a7c824f7c417f37
Rohan <rohn.py@gmail.com>
2025-07-25 21:42:55 +0530

    done with user-authentication,profile,update

---
+++ ecommerce_api/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.http import JsonResponse
+
+
+
+def root_view(request):
+    return JsonResponse({"message": "E-commerce API is running ✅"})
+
+
+urlpatterns = [
+    path("", root_view),
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
